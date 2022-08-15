5309d1a99ed30fff31ec456ba193f21fa97693a2
otienosteve <steveotieno701@gmail.com>
2022-08-15 11:00:29 +0300

    commit tree e0963451c50ed8dcb49047827a307adf48371f8b

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,24 @@
 class Dog
+    attr_accessor :name, :breed, :id
+    def initialize (name:,breed:,id:nil)
+        @name=name
+        @breed=breed
+        @id=id
+
+    end
+    def self.create_table()
+        sql = <<-SQL
+        CREATE TABLE IF NOT EXISTS songs (
+          id INTEGER PRIMARY KEY,
+          name TEXT,
+          breed TEXT
+        )
+      SQL
+  
+      DB[:conn].execute(sql)
+        
+    end
+    def self.drop_table()
+    end
 
 end
